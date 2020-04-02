254dbd0f16e4f6a359bde7a78e45942f96f4d519
unknown <arteg132579@gmail.com>
2020-04-02 21:39:19 +0300

    refactor: add eslint configuration file

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 window.onload = () => {
     let key, text;
-    let arrayOfRussianSymbols = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|', 'Del', 'Caps lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '', '', ''];
-    let body = document.querySelector('body');
-    let page = document.querySelector('html');
-    let meta = document.createElement('META');
-    let head = document.querySelector('HEAD');
-    let keyboard = document.createElement('DIV');
+    let arrayOfRussianSymbols = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|', 'Delete', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift', 'Control', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+    const body = document.querySelector('body'),
+        keyboard = document.createElement('DIV'),
+        arrOfKeys = keyboard.childNodes;
+    let elem;
     keyboard.classList.add('keyboard-wrap');
     body.append(keyboard);
-    head.append(meta);
-    meta.setAttribute('charset', 'utf-8');
-    page.lang = 'ru';
     arrayOfRussianSymbols.forEach(elem => {
         key = document.createElement('DIV');
         text = document.createTextNode(elem);
@@ -20,9 +16,29 @@ window.onload = () => {
     });
     document.addEventListener('keydown', event => {
         let indexOfPushedButton = arrayOfRussianSymbols.indexOf(event.key);
-        let arrOfKeys = keyboard.childNodes;
-        let elem = arrOfKeys[indexOfPushedButton];
+        elem = arrOfKeys[indexOfPushedButton];
+        specialButtonsSelector();
         elem.classList.add('pushed-button');
     });
-
+    document.addEventListener('keyup', event => {
+        let indexOfPushedButton = arrayOfRussianSymbols.indexOf(event.key);
+        elem = arrOfKeys[indexOfPushedButton];
+        specialButtonsSelector();
+        elem.classList.remove('pushed-button');
+    });
+    function specialButtonsSelector() {
+        if (event.code == 'ControlLeft') {
+            elem = arrOfKeys[55];
+        } else if (event.code == 'ControlRight') {
+            elem = arrOfKeys[60];
+        } else if (event.code == 'ShiftLeft') {
+            elem = arrOfKeys[42];
+        } else if (event.code == 'ShiftRight') {
+            elem = arrOfKeys[54];
+        } else if (event.code == 'AltRight') {
+            elem = arrOfKeys[59];
+        } else if (event.code == 'AltLeft') {
+            elem = arrOfKeys[57];
+        }
+    }
 }
