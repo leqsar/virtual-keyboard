eddc0a4efe4302d80dfb62dc6c44503adb140b44
unknown <arteg132579@gmail.com>
2020-04-05 14:07:33 +0300

    feat: add textarea generation and posibility of changing language

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 window.onload = () => {
-    let key, text;
-    let arrayOfRussianSymbols = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|', 'Delete', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift', 'Control', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+    let key, text, elem;
+    let arrayOfRussianSymbols = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|', 'Delete', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift', 'Control', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
+        arrayOfEnglishSymbols = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|', 'Delete', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
+        language = 'english';
     const body = document.querySelector('body'),
         keyboard = document.createElement('DIV'),
-        arrOfKeys = keyboard.childNodes;
-    let elem;
-    keyboard.classList.add('keyboard-wrap');
+        arrOfKeys = keyboard.childNodes,
+        textarea = document.createElement('textarea');
+    body.append(textarea);
     body.append(keyboard);
-    arrayOfRussianSymbols.forEach(elem => {
-        key = document.createElement('DIV');
-        text = document.createTextNode(elem);
-        keyboard.append(key);
-        key.append(text);
-        key.classList.add('key');
-    });
+    textarea.setAttribute('readonly', 'readonly');
+    keyboard.classList.add('keyboard-wrap');
+
+    if (language == 'russian') {
+        keyGeneration(arrayOfRussianSymbols);
+    } else {
+        keyGeneration(arrayOfEnglishSymbols);
+    }
+
     document.addEventListener('keydown', event => {
-        let indexOfPushedButton = arrayOfRussianSymbols.indexOf(event.key);
-        elem = arrOfKeys[indexOfPushedButton];
-        specialButtonsSelector();
+        if ((event.ctrlKey) && (event.shiftKey)) {
+            language = language == 'russian'? 'english':'russian';
+            language == 'russian'? swapLanguage(arrayOfRussianSymbols):swapLanguage(arrayOfEnglishSymbols);
+        }
+        defineButton();
         elem.classList.add('pushed-button');
+        textOutput();
     });
     document.addEventListener('keyup', event => {
-        let indexOfPushedButton = arrayOfRussianSymbols.indexOf(event.key);
-        elem = arrOfKeys[indexOfPushedButton];
-        specialButtonsSelector();
+        defineButton();
         elem.classList.remove('pushed-button');
     });
+
+    function keyGeneration(arrayOfSymbols) {
+        arrayOfSymbols.forEach(elem => {
+            key = document.createElement('DIV');
+            text = document.createTextNode(elem);
+            keyboard.append(key);
+            key.append(text);
+            key.classList.add('key');
+        });
+    }
+
     function specialButtonsSelector() {
         if (event.code == 'ControlLeft') {
             elem = arrOfKeys[55];
@@ -40,5 +56,21 @@ window.onload = () => {
         } else if (event.code == 'AltLeft') {
             elem = arrOfKeys[57];
         }
+    }
+
+    function swapLanguage(arrayOfSymbols) {
+        arrOfKeys.forEach((item, i) => {
+            item.textContent = `${arrayOfSymbols[i]}`;
+        });
+    }
+
+    function textOutput() {
+        textarea.textContent = `${textarea.textContent+elem.textContent}`;
+    }
+
+    function defineButton() {
+        let indexOfPushedButton = arrayOfRussianSymbols.indexOf(event.key) == -1?arrayOfEnglishSymbols.indexOf(event.key):arrayOfRussianSymbols.indexOf(event.key);
+        elem = arrOfKeys[indexOfPushedButton];
+        specialButtonsSelector();
     }
 }
