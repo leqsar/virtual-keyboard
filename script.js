2b22d07406e47a31cc38721fea734e6e9ed389d9
unknown <arteg132579@gmail.com>
2020-04-05 14:23:46 +0300

    feat: add the ability to save language to the session storage

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = () => {
     let key, text, elem;
     let arrayOfRussianSymbols = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|', 'Delete', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift', 'Control', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
         arrayOfEnglishSymbols = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|', 'Delete', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
-        language = 'english';
+        language = sessionStorage.getItem('language') == null? 'english': sessionStorage.getItem('language');
     const body = document.querySelector('body'),
         keyboard = document.createElement('DIV'),
         arrOfKeys = keyboard.childNodes,
@@ -22,6 +22,7 @@ window.onload = () => {
         if ((event.ctrlKey) && (event.shiftKey)) {
             language = language == 'russian'? 'english':'russian';
             language == 'russian'? swapLanguage(arrayOfRussianSymbols):swapLanguage(arrayOfEnglishSymbols);
+            sessionStorage.setItem('language',`${language}`);
         }
         defineButton();
         elem.classList.add('pushed-button');
