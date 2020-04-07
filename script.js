ca2680816e55ab96b3c0d274caa48a2f5bc3c293
unknown <arteg132579@gmail.com>
2020-04-07 22:42:58 +0300

    feat: add representaition of the language on page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     let key, text, elem,
-        russianSymbols = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|', 'Delete', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift', 'Control', 'Win', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
+        russianSymbols = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', `\x5c`, 'Delete', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift', 'Control', 'Win', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
         englishSymbols = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|', 'Delete', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Win', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
         specialButtons = ['Tab', 'Shift', 'Meta', 'Alt', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Enter', 'Backspace', 'Delete', 'CapsLock', 'Control'],
         language = sessionStorage.getItem('language') == null ? 'english' : sessionStorage.getItem('language'),
@@ -14,10 +14,13 @@ window.onload = () => {
     const body = document.querySelector('body'),
         keyboard = document.createElement('DIV'),
         textarea = document.createElement('textarea'),
+        languageContainer = document.createElement('SPAN'),
         arrOfKeys = keyboard.childNodes;
+    body.append(languageContainer);
     body.append(textarea);
     body.append(keyboard);
     keyboard.classList.add('keyboard-wrap');
+    languageContainer.textContent = `${language}`;
 
     if (language == 'russian') {
         keyGeneration(russianSymbols);
@@ -38,30 +41,6 @@ window.onload = () => {
         if (specialButtons.indexOf(event.key) !== -1) {
             event.preventDefault();
         }
-        if (event.key == 'ArrowLeft') {
-            setCaret(textarea.selectionEnd-1);
-            isCaretShift = true;
-        }
-        if (event.key == 'ArrowRight') {
-            setCaret(textarea.selectionEnd+1);
-            isCaretShift = true;
-        }
-        if (event.key == 'ArrowUp') {
-            let content = textarea.textContent.split(''),
-                indexesOfLineBreaks = [],
-                caretPosition = textarea.selectionEnd;
-            content.forEach((item, i) => {
-                if (item == "\n") indexesOfLineBreaks.push(i);
-            });
-            indexesOfLineBreaks.filter(elem => elem < caretPosition);
-            let length = indexesOfLineBreaks.length;
-            let caretNewPosition;
-            if (length >= 2) {
-                caretNewPosition = indexesOfLineBreaks[length-2] + (caretPosition - indexesOfLineBreaks[length-1]);
-                setCaret(caretNewPosition);
-                isCaretShift = true;
-            }
-        }
         /*if (event.key == 'ArrowDown') {
             let content = textarea.textContent.split(''),
                 indexesOfLineBreaks = [],
@@ -78,24 +57,10 @@ window.onload = () => {
                 isCaretShift = true;
             }
         }*/
-        if (event.key == 'Backspace') {
-            let content = textarea.textContent.split(''),
-                caretPosition = textarea.selectionEnd;
-            content.splice(caretPosition-1, 1);
-            textarea.textContent = `${content.join('')}`;
-            setCaret(caretPosition-1);
-        }
-        if (event.key == 'Delete') {
-            let content = textarea.textContent.split(''),
-                caretPosition = textarea.selectionEnd;
-            if (caretPosition < textarea.textContent.length) {
-                content.splice(caretPosition, 1);
-                textarea.textContent = `${content.join('')}`;
-                setCaret(caretPosition);
-            }
-        }
         isShift = event.getModifierState("Shift") ? true : false;
         isCapsLock = event.getModifierState("CapsLock") ? true : false;
+        arrowButtons();
+        deleteButtons();
         defineButton();
         elem.classList.add('pushed-button');
         animation(elem);
@@ -152,10 +117,56 @@ window.onload = () => {
         }
     }
 
+    function deleteButtons() {
+        let content = textarea.textContent.split(''),
+            caretPosition = textarea.selectionEnd;
+        if (event.key == 'Backspace') {
+            content.splice(caretPosition-1, 1);
+            textarea.textContent = `${content.join('')}`;
+            setCaret(caretPosition-1);
+        }
+        if (event.key == 'Delete') {
+            if (caretPosition < textarea.textContent.length) {
+                content.splice(caretPosition, 1);
+                textarea.textContent = `${content.join('')}`;
+                setCaret(caretPosition);
+            }
+        }
+    }
+
+    function arrowButtons() {
+        if (event.key == 'ArrowLeft') {
+            setCaret(textarea.selectionEnd-1);
+            isCaretShift = true;
+        }
+        if (event.key == 'ArrowRight') {
+            setCaret(textarea.selectionEnd+1);
+            isCaretShift = true;
+        }
+        if (event.key == 'ArrowUp') {
+            let content = textarea.textContent.split(''),
+                indexesOfLineBreaks = [],
+                caretPosition = textarea.selectionEnd;
+            content.forEach((item, i) => {
+                if (item == "\n") indexesOfLineBreaks.push(i);
+            });
+            indexesOfLineBreaks.filter(elem => elem < caretPosition);
+            indexesOfLineBreaks.splice(0, 0, 1);
+            let length = indexesOfLineBreaks.length;
+            let caretNewPosition;
+            if (length >= 2) {
+                caretNewPosition = indexesOfLineBreaks[length-2] + (caretPosition - indexesOfLineBreaks[length-1]);
+                setCaret(caretNewPosition);
+                isCaretShift = true;
+            }
+        }
+    }
+
     function swapLanguage(arrayOfSymbols) {
         arrOfKeys.forEach((item, i) => {
             item.textContent = `${arrayOfSymbols[i]}`;
         });
+        languageContainer.textContent = `${language}`;
     }
 
     function textOutput(pressedButton) {
