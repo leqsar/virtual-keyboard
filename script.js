fae8010ecb87add10cf192075933225845b7a6f2
unknown <arteg132579@gmail.com>
2020-04-07 12:01:02 +0300

    fix: remove text output from special buttons; feat: add tab and line break by click on the button

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ window.onload = () => {
     let key, text, elem;
     let arrayOfRussianSymbols = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|', 'Delete', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift', 'Control', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
         arrayOfEnglishSymbols = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|', 'Delete', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
-        language = sessionStorage.getItem('language') == null? 'english': sessionStorage.getItem('language');
+        language = sessionStorage.getItem('language') == null ? 'english' : sessionStorage.getItem('language'),
+        isCapsLock = false,
+        specialButtons = ['Tab', 'Shift', 'Meta', 'Alt', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Enter', 'Backspace', 'Delete', 'CapsLock', 'Control'];
     const body = document.querySelector('body'),
         keyboard = document.createElement('DIV'),
         arrOfKeys = keyboard.childNodes,
         textarea = document.createElement('textarea');
+    let specialSymbols = {
+        'Tab':'\t',
+        'Enter':'\n'
+    }
     body.append(textarea);
     body.append(keyboard);
     textarea.setAttribute('readonly', 'readonly');
@@ -20,11 +26,15 @@ window.onload = () => {
 
     document.addEventListener('keydown', event => {
         if ((event.ctrlKey) && (event.shiftKey)) {
-            language = language == 'russian'? 'english':'russian';
-            language == 'russian'? swapLanguage(arrayOfRussianSymbols):swapLanguage(arrayOfEnglishSymbols);
+            language = language == 'russian' ? 'english' : 'russian';
+            language == 'russian' ? swapLanguage(arrayOfRussianSymbols) : swapLanguage(arrayOfEnglishSymbols);
             sessionStorage.setItem('language',`${language}`);
         }
+        if (specialButtons.indexOf(event.key) !== -1) event.preventDefault();
         defineButton();
+        if (event.key == 'CapsLock') {
+            isCapsLock = isCapsLock == true ? false : true;
+        }
         elem.classList.add('pushed-button');
         textOutput(elem);
     });
@@ -73,11 +83,21 @@ window.onload = () => {
     }
 
     function textOutput(pressedButton) {
-        textarea.textContent = `${textarea.textContent+pressedButton.textContent}`;
+        if (specialSymbols[event.key] !== undefined) {
+            textarea.textContent = `${textarea.textContent+specialSymbols[event.key]}`;
+        } else if (specialButtons.indexOf(event.key) !== -1) {
+            textarea.textContent = `${textarea.textContent}`;
+        } else if (isCapsLock) {
+            textarea.textContent = `${textarea.textContent+pressedButton.textContent.toUpperCase()}`;
+        } else {
+            textarea.textContent = `${textarea.textContent+pressedButton.textContent}`;
+        }
     }
 
     function defineButton() {
-        let indexOfPushedButton = arrayOfRussianSymbols.indexOf(event.key) == -1?arrayOfEnglishSymbols.indexOf(event.key):arrayOfRussianSymbols.indexOf(event.key);
+        let key = (isCapsLock) && (event.key !== 'CapsLock') ? event.key.toLowerCase() : event.key,
+            indexOfPushedButton = arrayOfRussianSymbols.indexOf(key) == -1 ? arrayOfEnglishSymbols.indexOf(key) : arrayOfRussianSymbols.indexOf(key);
+            console.log(key);
         elem = arrOfKeys[indexOfPushedButton];
         specialButtonsSelector();
     }
